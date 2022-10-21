e09f7c6890b06a18c20c5eb536cfb7a0c48cc707
John Papwinter <johnpapahimonas@yahoo.com>
2022-10-21 23:37:18 +0300

    added thymeleaf layouts

--- src/main/java/com/shoppingcartapp/controller/ItemController.java
+++ src/main/java/com/shoppingcartapp/controller/ItemController.java
@@ -1,10 +1,7 @@
 package com.shoppingcartapp.controller;
 
-import com.shoppingcartapp.model.Item;
-import com.shoppingcartapp.service.ShoppingCartServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -14,7 +11,6 @@ public class ItemController {
     public ItemController() {
     }
 
-    // GET ALL
     @GetMapping("/")
     public String viewHomePage() {
         return "index";
