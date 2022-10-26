09e8db69a2f584254b4bd5d82ada335ccbb0e7b0
John Papwinter <johnpapahimonas@yahoo.com>
2022-10-26 22:18:22 +0300

    added in-memory security authentication

---
+++ src/main/java/com/shoppingcartapp/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.shoppingcartapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .hasRole("USER")
+                .antMatchers("/login*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+//                .loginPage("/login")
+                .defaultSuccessUrl("/");
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
