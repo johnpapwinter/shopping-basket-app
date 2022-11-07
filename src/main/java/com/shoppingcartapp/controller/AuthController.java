f91f341ea54e70c0f7636d0656c9991996c17a8d
John Papwinter <johnpapahimonas@yahoo.com>
2022-11-07 08:02:24 +0200

    added user registration

--- src/main/java/com/shoppingcartapp/controller/AuthController.java
+++ src/main/java/com/shoppingcartapp/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.shoppingcartapp.controller;
 
+import com.shoppingcartapp.security.dto.RegistrationDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +14,8 @@ public class AuthController {
     }
 
     @GetMapping("/registration")
-    public String viewRegistrationPage() {
+    public String viewRegistrationPage(Model model) {
+        model.addAttribute("userRegistration", new RegistrationDTO());
         return "registration";
     }
 }
